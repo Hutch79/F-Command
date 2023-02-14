1adfa407575d2a55f8ede392e6911c20f4db6d38
Hutch79 <andrin.monn@gmail.com>
2023-02-14 22:47:49 +0700

    Initial commit

---
+++ src/main/java/ch/hutch79/EventListener.java
@@ -0,0 +1,70 @@
+package ch.hutch79;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerSwapHandItemsEvent;
+import java.util.*;
+
+public class EventListener implements Listener {
+
+    private final Main mainClass = Main.getInstance();
+    List<String> commandOptions;
+
+    public EventListener() {
+
+        Set<String> commandOptions2 = Objects.requireNonNull(mainClass.getConfig().getConfigurationSection("command")).getKeys(false);
+
+        commandOptions = new ArrayList<>(commandOptions2.size());
+        commandOptions.addAll(commandOptions2);
+
+    }
+
+    public String getInfo(int count, String value){
+
+        try {
+            return Objects.requireNonNull(mainClass.getConfig().getString("command." + commandOptions.get(count-1) + "." + value));
+        }
+        catch (NullPointerException e) {
+            mainClass.getLogger().warning(value + " could not be fount in configuration of command " + commandOptions.get(count-1));
+            return "ijfuwiqehfzqgw9r8mhe0w87gh";
+        }
+    }
+
+    @EventHandler
+    public void onSwapHandItemsEvent(PlayerSwapHandItemsEvent e) {
+
+        Player player = e.getPlayer();
+
+        int count = 0;
+        while (count < commandOptions.size()) {
+            count++;
+
+            if (!getInfo(count, "permission").equalsIgnoreCase("None")) { // Correct Permission?
+                if (!player.hasPermission(getInfo(count, "permission"))) {
+                    return;
+                }
+            }
+
+            if (getInfo(count, "requireShift").equalsIgnoreCase("true")) {
+                if (!player.isSneaking()) {
+                    return;
+                }
+            }
+
+
+
+
+
+            if (getInfo(count, "cancel").equalsIgnoreCase("true")) {
+                e.setCancelled(true);
+            }
+
+             player.performCommand(getInfo(count, "command"));
+
+
+        }
+
+    }
+
+}
---
+++ src/main/java/ch/hutch79/Main.java
@@ -0,0 +1,52 @@
+package ch.hutch79;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+    PluginDescriptionFile pdf = this.getDescription();
+    private static Main instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        Bukkit.getPluginManager().registerEvents(new EventListener(), this);
+
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+
+        if (pdf.getVersion().contains("Beta")) {
+            getLogger().warning("It seems you're using a dev Build");
+            getLogger().warning("You can use this Build on Production Servers but for some reasons i would not recommend that.");
+            getLogger().warning("Mostly i added some new features or have changed some things and need feedback before releasing.");
+            getLogger().warning("So if you want to provide Feedback for this Version, don't hesitate to do so on GitHub");
+            getLogger().warning("If you find any Bugs, please report them on GitHub: https://github.com/Hutch79/CookieClicker");
+        }
+
+        Bukkit.getConsoleSender().sendMessage("§dF-Command §8> §5======================================================");
+        Bukkit.getConsoleSender().sendMessage("§dF-Command §8> §5| §6F-Command " + pdf.getVersion() + " §bby Hutch79");
+        Bukkit.getConsoleSender().sendMessage("§dF-Command §8> §5| §7Has been §2Enabled");
+        Bukkit.getConsoleSender().sendMessage("§dF-Command §8> §5------------------------------------------------------");
+        Bukkit.getConsoleSender().sendMessage("§dF-Command §8> §5| §cIf you find any Bugs, please report them on GitHub");
+        Bukkit.getConsoleSender().sendMessage("§dF-Command §8> §5| §6https://github.com/Hutch79/CookieClicker");
+        Bukkit.getConsoleSender().sendMessage("§dF-Command §8> §5======================================================");
+    }
+
+    @Override
+    public void onDisable() {
+        Bukkit.getConsoleSender().sendMessage("§dF-Command §8> §5======================================================");
+        Bukkit.getConsoleSender().sendMessage("§dF-Command §8> §5| §6F-Command " + pdf.getVersion() + " §bby Hutch79");
+        Bukkit.getConsoleSender().sendMessage("§dF-Command §8> §5| §7Has been §cDisabled");
+        Bukkit.getConsoleSender().sendMessage("§dF-Command §8> §5------------------------------------------------------");
+        Bukkit.getConsoleSender().sendMessage("§dF-Command §8> §5| §cIf you find any Bugs, please report them on GitHub");
+        Bukkit.getConsoleSender().sendMessage("§dF-Command §8> §5| §6https://github.com/Hutch79/F-Command");
+        Bukkit.getConsoleSender().sendMessage("§dF-Command §8> §5======================================================");
+    }
+
+    public static Main getInstance() {
+        return instance;
+    }
+
+}
