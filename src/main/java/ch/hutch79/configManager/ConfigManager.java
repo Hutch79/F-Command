eb7141c704b00983a873474393bb18635e8aff34
Hutch79 <me@hutch79.ch>
2024-02-01 07:35:32 +0100

    Cleaned up ConfigManager and added writeConfig (not tested yet)

--- src/main/java/ch/hutch79/configManager/ConfigManager.java
+++ src/main/java/ch/hutch79/configManager/ConfigManager.java
@@ -1,6 +1,5 @@
 package ch.hutch79.configManager;
 
-import ch.hutch79.configManager.configClass.config.v0.Config;
 import ch.hutch79.utility.Debugger;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -13,43 +12,37 @@ import java.io.IOException;
 public class ConfigManager {
 
     private static String pluginPath;
-
+    private ObjectMapper writeMapper;
+    private ObjectMapper readMapper;
     public ConfigManager(File _pluginPath) {
-        this.pluginPath = _pluginPath.toString();
+        pluginPath = _pluginPath.toString();
+
+        writeMapper = new ObjectMapper(new YAMLFactory().disable(YAMLGenerator.Feature.WRITE_DOC_START_MARKER));
+        writeMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        writeMapper.findAndRegisterModules();
+
+        readMapper = new ObjectMapper(new YAMLFactory());
+        readMapper.findAndRegisterModules();
     }
 
 
-//    public void configStuff () {
-//        ObjectMapper mapper = new ObjectMapper(new YAMLFactory().disable(YAMLGenerator.Feature.WRITE_DOC_START_MARKER));
-//        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-//        mapper.findAndRegisterModules();
-//
-////        mapper.readValue(new File("test.yaml"), Config.class);
-//
-//        Config config = new Config();
-//        config.setVersion(1);
-//        config.setDebug(false);
-//
-//
-//        try {
-//            mapper.writeValue(new File(pluginPath + "/test.yaml"), config);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
+    public void writeConfig (Class<?> configClass, String localPath) {
+        try {
+            writeMapper.writeValue(new File(pluginPath + "/" + localPath), configClass);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public <T> T getConfig (Class<?> configClass, String localPath) {
-        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-        mapper.findAndRegisterModules();
-
-        T hui;
+        T config;
         try {
             Debugger.debug(pluginPath + "/" + localPath);
-            hui = (T) mapper.readValue(new File(pluginPath + "/" + localPath), configClass);
+            config = (T) readMapper.readValue(new File(pluginPath + "/" + localPath), configClass);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        return hui;
+        return config;
     }
 }
